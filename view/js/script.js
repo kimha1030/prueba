3c313275ff66095affff2635b8dfdd46f63147b2
kimha1030 <kimha1030@gmail.com>
2022-10-03 23:44:14 -0500

    Se agrega funcion printData, que permite mostrar la informacion requerida

--- view/js/script.js
+++ view/js/script.js
@@ -7,11 +7,25 @@ function getResponse() {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
+        printData(data);
       })
       .catch(function (err) {
         console.log("Error", err);
       });
+  });
+}
+
+function printData(data) {
+  let dataInfo = data.data.result;
+  let containerResult = document.getElementById("containerResult");
+  dataInfo.map((item) => {
+    let id = item.id;
+    let contact = item.contact_no;
+    let lastname = item.lastname;
+    let createdTime = item.createdtime;
+    let paragraph = document.createElement("p");
+    paragraph.innerHTML = `id: ${id}, Contact: ${contact}, Lastname: ${lastname}, Date created: ${createdTime}`;
+    containerResult.append(paragraph);
   });
 }
 
