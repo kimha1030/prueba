6ee17e8f78ce9142660be4b137784e2e1c317bc8
kimha1030 <kimha1030@gmail.com>
2022-10-03 23:19:00 -0500

    Se agrega funcion loadData al controlador que retorna los datos del token desde la api (metodo getToken)

---
+++ api/api.php
@@ -0,0 +1,24 @@
+<?php
+
+class Api
+{
+    public function getToken() {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://develop.datacrm.la/anieto/anietopruebatecnica/webservice.php?operation=getchallenge&username=prueba',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        echo $response;
+    }
+}
+?>
--- controller/contentController.php
+++ controller/contentController.php
@@ -1,9 +1,17 @@
 <?php
+require_once "api/api.php";
 
 class contentController
 {
     public function index() {       
         require_once "view/content.php";
+    }
+
+    public function loadData(){
+        $api = new Api();
+
+        $data = $api->getToken();
+        $tokenString = $data["result"]["token"];
     }
 }
 
