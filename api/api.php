43d6ff81fb13180475b89b7a894adc749af81be1
kimha1030 <kimha1030@gmail.com>
2022-10-04 09:09:36 -0500

    Se crea funcion executeCurl y se emplea en las demas funciones donde se envian los parametros de url, method y postoptions (solo si el metodo es POST)

--- api/api.php
+++ api/api.php
@@ -3,63 +3,53 @@
 class Api
 {
     public function getToken() {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://develop.datacrm.la/anieto/anietopruebatecnica/webservice.php?operation=getchallenge&username=prueba',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
-        ));
-
-        $response = curl_exec($curl);
-        curl_close($curl);
-
-        return json_decode($response,true);
+        $url = "https://develop.datacrm.la/anieto/anietopruebatecnica/webservice.php?operation=getchallenge&username=prueba";
+        $method = "GET";
+        $response = $this->executeCurl($url, $method);
+        return $response;
     }
 
     public function getSessionName($tokenString) {
+        $url = "https://develop.datacrm.la/anieto/anietopruebatecnica/webservice.php";
+        $method = "POST";
         $accessKey = md5($tokenString."3DlKwKDMqPsiiK0B");
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://develop.datacrm.la/anieto/anietopruebatecnica/webservice.php',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => "operation=login&username=prueba&accessKey=".$accessKey,
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/x-www-form-urlencoded'
-        ),
-        ));
-
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return json_decode($response,true);
+        $postOptions = array(
+            CURLOPT_POSTFIELDS => 'operation=login&username=prueba&accessKey='.$accessKey,
+            CURLOPT_HTTPHEADER => array('Content-Type: application/x-www-form-urlencoded'),
+        );
+        $response = $this->executeCurl($url, $method, $postOptions);
+        return $response;
     }
 
     public function getData($sessionName) {
+        $url = "https://develop.datacrm.la/anieto/anietopruebatecnica/webservice.php?operation=query&sessionName=".$sessionName."&query=select%20*%20from%20Contacts;";
+        $method = "GET";
+        $response = $this->executeCurl($url, $method);
+        return $response;
+    }
+
+    public function executeCurl($url, $method, $postOptions="") {
         $curl = curl_init();
 
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://develop.datacrm.la/anieto/anietopruebatecnica/webservice.php?operation=query&sessionName=".$sessionName."&query=select%20*%20from%20Contacts;",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
-        ));
+        $options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => $method,
+        );
+        
+        if ($method == "POST") {
+            $joinOptions = $options + $postOptions;
+            curl_setopt_array($curl, $joinOptions);
+        } else {
+            curl_setopt_array($curl, $options);
+        }
 
         $response = curl_exec($curl);
-
         curl_close($curl);
         return json_decode($response,true);
     }
